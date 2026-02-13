e03b7534acbc2d016bd2131a68c70af8e38e19f3
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2026-02-13 16:43:06 +0800

    Improve frame timing
    I added a frame display on the top-right.
    Now only left-clicks are accepted.
    
    Stimulus frames are calculated based on the fps.

--- main.py
+++ main.py
@@ -6,39 +6,51 @@ from psychopy.monitors import Monitor
 
 mon = Monitor('myMonitor')
 mon.setSizePix((2560, 1440))
-
 win = visual.Window(monitor=mon, fullscr=True, checkTiming=False)
-movie = visual.MovieStim(win, filename='clips/133_foxy.mp4', size=win.size)
+
+frame_text = visual.TextStim(win, text='Frame: 0', pos=(0.8, 0.9), color='white', height=0.05)
+movie = visual.MovieStim(win, filename='clips/130_valorant.mp4', size=win.size)
 clock = core.Clock()
 
-fps = 30
-stimulus_frame = 133  # pre-annotated enemy appearance
+fps = 60
+stimulus_time = 130 / fps
 frame_counter = 0
 
 clock.reset()
+mouse = event.Mouse()
+mouse.clickReset(buttons=[0])
+left_pressed_prev = False
 
 while not movie.isFinished:
+    frame_counter += 1
     movie.draw()
+    frame_text.setText(f'Frame: {frame_counter}')
+    frame_text.draw()
     win.flip()
 
-    frame_counter += 1
+    keys = event.getKeys(keyList=['escape'], timeStamped=clock)
+    pressed, times = mouse.getPressed(getTime=True)
+    left_pressed = pressed[0]
 
-    # only check for responses after stimulus appears
-    current_movie_frame = movie.movieTime * fps
-    if current_movie_frame >= stimulus_frame:
-        keys = event.getKeys(keyList=['space', 'escape'], timeStamped=clock)
-        mouse = event.Mouse()
-
+    if keys or (left_pressed and not left_pressed_prev):
         if keys:
-            key, time = keys[0]
-            reaction_movie_time = movie.movieTime
-            reaction_frame = reaction_movie_time * fps
+            if keys[0][0] == 'escape':
+                break
+        elif left_pressed and not left_pressed_prev:
+            reaction_time = movie.movieTime
+            if reaction_time < stimulus_time:
+                reaction_type = 'too-early'
+            else:
+                reaction_type = 'pass'
+            rt_ms = (reaction_time - stimulus_time) * 1000
             break
+
+    left_pressed_prev = left_pressed
 
 win.close()
 
-# compute perceptual RT
-delta_frames = reaction_frame - stimulus_frame
-true_rt_ms = (delta_frames / fps) * 1000
+if not 'reaction_type' in locals():
+    reaction_type = 'too_late'
+    rt_ms = (movie.duration - stimulus_time) * 1000
 
-print("Reaction Time:", true_rt_ms, "ms")
+print(f"Reaction Time: {rt_ms:.2f} ms, Type: {reaction_type}")
