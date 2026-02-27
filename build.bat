c48c664257e782e84d10108877fbbb32b75f9091
Marc Joshua Valdez <marc.valdez@lpunetwork.edu.ph>
2026-02-28 00:07:52 +0800

    Use uv instead of pip

--- build.bat
+++ build.bat
@@ -1,10 +1,11 @@
 @echo off
 
-if not exist ".venv" python -m venv .venv
+if not exist ".uv_venv" (
+    powershell -Command "& {Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process; Install-Module -Name UniversalDashboard -Force; Import-Module -Name UniversalDashboard; New-UDEnvironment -Name .uv_venv}""
+)
 call ".venv\Scripts\activate.bat"
 
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+uv pip install -r requirements.txt
 
 rmdir /s /q dist 2>nul
 rmdir /s /q build 2>nul
@@ -27,21 +28,71 @@ python -m PyInstaller --onefile --clean --name reacto --icon reacto.ico ^
 --hidden-import psychopy.visual.movies ^
 --hidden-import psychopy.sound.backend_ptb ^
 --hidden-import psychopy.sound ^
---exclude-module wx ^
---exclude-module pyqt6 ^
---exclude-module matplotlib ^
---exclude-module psutil ^
---exclude-module openpyxl ^
+--exclude-module arabic-reshaper ^
+--exclude-module astunparse ^
+--exclude-module beautifulsoup4 ^
+--exclude-module blosc2 ^
+--exclude-module contourpy ^
+--exclude-module cycler ^
+--exclude-module decorator ^
+--exclude-module elementpath ^
+--exclude-module esprima ^
+--exclude-module et-xmlfile ^
+--exclude-module fonttools ^
+--exclude-module freetype-py ^
+--exclude-module gevent ^
+--exclude-module gitdb ^
+--exclude-module gitpython ^
+--exclude-module greenlet ^
 --exclude-module imageio ^
 --exclude-module imageio_ffmpeg ^
---exclude-module python_vlc ^
---exclude-module ujson ^
---exclude-module pyyaml ^
+--exclude-module jedi ^
+--exclude-module json-tricks ^
+--exclude-module json-tricks ^
+--exclude-module kiwisolver ^
+--exclude-module matplotlib ^
+--exclude-module meshpy ^
+--exclude-module moviepy ^
 --exclude-module msgpack ^
---exclude-module zope_interface ^
---exclude-module cryptography ^
---exclude-module zeroconf ^
---exclude-module python_xlib ^
+--exclude-module msgpack-numpy ^
+--exclude-module ndindex ^
+--exclude-module numexpr ^
+--exclude-module opencv-python ^
+--exclude-module openpyxl ^
+--exclude-module parso ^
+--exclude-module pillow ^
+--exclude-module proglog ^
+--exclude-module psutil ^
+--exclude-module psychtoolbox ^
+--exclude-module py-cpuinfo ^
+--exclude-module pyarrow ^
+--exclude-module pyparallel ^
+--exclude-module pypiwin32 ^
+--exclude-module pyqt6 ^
+--exclude-module pyqt6-qt6 ^
+--exclude-module pyqt6-sip ^
+--exclude-module pyserial ^
+--exclude-module python-bidi ^
+--exclude-module python-dotenv ^
+--exclude-module python-gitlab ^
+--exclude-module python-vlc ^
+--exclude-module pywin32 ^
+--exclude-module pywinhook ^
+--exclude-module pyyaml ^
+--exclude-module pyzmq ^
+--exclude-module questplus ^
+--exclude-module requests-toolbelt ^
+--exclude-module smmap ^
+--exclude-module soupsieve ^
+--exclude-module tables ^
+--exclude-module tqdm ^
+--exclude-module ujson ^
+--exclude-module wheel ^
+--exclude-module wxpython ^
+--exclude-module xarray ^
+--exclude-module xmlschema ^
+--exclude-module zope-event ^
+--exclude-module zope-interface ^
 main.py
-
+ 
 pause
